0f1ef584286ac45126e0b6962fc3dc2789a2d405
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-29 18:35:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,12 @@ class Application
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
-
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+    end
+    resp.finish
   end
 
 
